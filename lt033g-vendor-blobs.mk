7bc433cf38c7f4c42a618fbd4696644d13d260d8
ShevT <shev_t@mail.ru>
2016-10-26 22:44:12 +0300

    camera: rename camera vendor...
    Associated with the removal camera wrapper

--- lt033g-vendor-blobs.mk
+++ lt033g-vendor-blobs.mk
@@ -70,7 +70,7 @@ PRODUCT_COPY_FILES += \
 
 # lib/hw
 PRODUCT_COPY_FILES += \
-    $(VENDOR_DIR)/lib/hw/camera.universal5420.so:system/lib/hw/camera.vendor.universal5420.so \
+    $(VENDOR_DIR)/lib/hw/camera.universal5420.so:system/lib/hw/camera.universal5420.so \
     $(VENDOR_DIR)/lib/hw/gps.universal5420.so:system/lib/hw/gps.universal5420.so \
     $(VENDOR_DIR)/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
 
